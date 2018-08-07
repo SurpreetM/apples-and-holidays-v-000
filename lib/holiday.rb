4840ac00c3fecbd8e02417b2e1b3b97dee0b800a
surpreet.mundra@gmail.com <surpreet.mundra@gmail.com>
2018-08-07 15:35:06 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,10 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-
+  holiday_hash.each do |season, holiday_name|
+    holiday_name.each do |holiday_name, supply|
+      puts "#{holiday_name}": "#{supply}"
+    end 
 end
 
 def all_holidays_with_bbq(holiday_hash)
